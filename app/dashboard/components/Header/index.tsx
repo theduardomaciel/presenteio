585e5c8fdd257b604cddf11c45d2c45921993afb
Eduardo <eduardomacielbr@gmail.com>
2022-12-13 00:24:59 -0300

    Before next-auth removal

--- app/dashboard/components/Header/index.tsx
+++ app/dashboard/components/Header/index.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Link from 'next/link';
 
 // Components
@@ -11,18 +9,24 @@ import styles from './header.module.css';
 // Icons
 import Logo from '../../../../public/logo.svg';
 
+// Auth
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from '../../../../pages/api/auth/[...nextauth]';
+
 type Props = React.HTMLAttributes<HTMLDivElement> & {
     profileChildren?: React.ReactNode;
 }
 
-export default function DashboardHeader({ profileChildren, children }: Props) {
+export default async function DashboardHeader({ profileChildren, children }: Props) {
+    const session = await unstable_getServerSession(authOptions);
+
     return (
         <div className={styles.container}>
             <Link href={`/dashboard`} className={styles.logo}>
                 <Logo />
             </Link>
             {children}
-            <DashboardProfile>
+            <DashboardProfile account={session?.user} >
                 {profileChildren}
             </DashboardProfile>
         </div>
