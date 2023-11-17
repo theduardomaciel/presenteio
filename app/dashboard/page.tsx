2f5336af0b115812ef9a753a86c337f5cb986fba
Eduardo Maciel <eduardomacielbr@gmail.com>
2023-11-17 11:49:42 -0300

    Remove prisma generate from build script

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,6 @@ import DashboardHeader from "@/dashboard/components/Header";
 import EventCard from "@/dashboard/components/Event/EventCard";
 import EmptyGuests from "@/dashboard/components/Guest/EmptyGuests";
 import CreateEventButton from "@/dashboard/components/CreateEventButton";
-import LandingFooter from "components/Landing/Footer/Footer";
 
 import { getEvents } from "lib/getEvents";
 
@@ -13,29 +12,26 @@ export default async function Dashboard() {
 	const events = await getEvents();
 
 	return (
-		<>
-			<div className={`${styles.container} min-h-screen`}>
-				<DashboardHeader profileChildren={<CreateEventButton />} />
-				<div className={`${styles.content}`}>
-					<h2 className={styles.title}>Meus Eventos</h2>
-					<div
-						className={`${styles.events} ${
-							events && events.length > 0 ? "" : styles.empty
-						}`}
-					>
-						{events && events?.length > 0 ? (
-							events?.map((event, index) => {
-								return <EventCard key={index} event={event} />;
-							})
-						) : (
-							<EmptyGuests
-								label={`Você ainda não criou nenhum evento.\nPara criar novos, clique no botão "Criar Evento" acima.`}
-							/>
-						)}
-					</div>
+		<div className={`${styles.container} min-h-screen`}>
+			<DashboardHeader profileChildren={<CreateEventButton />} />
+			<div className={`${styles.content}`}>
+				<h2 className={styles.title}>Meus Eventos</h2>
+				<div
+					className={`${styles.events} ${
+						events && events.length > 0 ? "" : styles.empty
+					}`}
+				>
+					{events && events?.length > 0 ? (
+						events?.map((event, index) => {
+							return <EventCard key={index} event={event} />;
+						})
+					) : (
+						<EmptyGuests
+							label={`Você ainda não criou nenhum evento.\nPara criar novos, clique no botão "Criar Evento" acima.`}
+						/>
+					)}
 				</div>
 			</div>
-			<LandingFooter className="!px-dashboard-wrapper" />
-		</>
+		</div>
 	);
 }
